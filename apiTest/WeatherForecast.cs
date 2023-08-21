f01aa0c0514547fea99f368a07cafb93adc37bfd
mayur <mayur>
2023-08-21 11:19:05 +0530

    add person.cs and modified weatherforeCast.cs

--- apiTest/WeatherForecast.cs
+++ apiTest/WeatherForecast.cs
@@ -4,6 +4,8 @@ namespace apiTest
 	{
 		public DateTime Date { get; set; }
 
+		public static float pi = 3.142F;
+
 		public int TemperatureC { get; set; }
 
 		public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
---
+++ apiTest/entities/person.cs
@@ -0,0 +1,8 @@
+﻿namespace apiTest.entities
+{
+	public class person
+	{
+		public int personId { get; set; }
+		public string personName { get; set; }
+	}
+}
